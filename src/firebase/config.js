0b3a7154c5c744b830abccece50c430e4f699af5
Hadis Sulejmani <hadissulejmani@outlook.com>
2022-08-28 17:33:08 +0200

    added firabase storage in config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
+import { getStorage } from "firebase/storage";
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCvBBNaEUS6sLUd8ZIukXBpWDAnFDwhoQs",
@@ -15,6 +15,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
+
+//timestamp
 const timestamp = db.timestamp;
 
-export { app, db, auth, timestamp };
+export { app, db, auth, storage, timestamp };
