848c189847ea1af0cee0f417ff54ef37ef5cd6a4
Hadis Sulejmani <hadissulejmani@outlook.com>
2022-08-29 13:05:14 +0200

    new route guards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
@@ -7,23 +7,34 @@ import Project from "./pages/project/Project";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import "./App.css";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Sidebar />
-        <div className="cotnainer">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/projects/:id" element={<Project />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className="cotnainer">
+            <Navbar />
+            <Routes>
+              {user && <Route path="/" element={<Dashboard />} />}
+              {user && <Route path="/create" element={<Create />} />}
+              {user && <Route path="/projects/:id" element={<Project />} />}
+              {!user && <Route path="/login" element={<Login />} />}
+              {!user && <Route path="/signup" element={<Signup />} />}
+              {!user && (
+                <Route
+                  path="*"
+                  element={<Navigate to="/login" replace={true} />}
+                />
+              )}
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
